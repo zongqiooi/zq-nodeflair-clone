7064a1a8baac608e5f20effddda0e95fc84b1e0e
zongqiooi <huro710@hotmail.com>
2023-08-21 01:40:04 +0800

    feat: complete pagination that changes job lists

--- src/components/JobPagination.jsx
+++ src/components/JobPagination.jsx
@@ -1,14 +1,23 @@
-const JobPagination = () => {
+/* eslint-disable react/prop-types */
+const JobPagination = ({ currentPage, setCurrentPageCallback }) => {
   return (
     <div className="job-pagination">
       <span className="page-text">
-        Page&nbsp;<strong>1&nbsp;</strong>of&nbsp;<strong>2</strong>
+        Page&nbsp;<strong>{currentPage}&nbsp;</strong>of&nbsp;<strong>2</strong>
       </span>
       <div className="page-square-buttons-container">
-        <div className="page-square-button" tabIndex="1">
+        <div
+          onClick={() => setCurrentPageCallback("1")}
+          className="page-square-button-1"
+          tabIndex="1"
+        >
           1
         </div>
-        <div className="page-square-button" tabIndex="1">
+        <div
+          onClick={() => setCurrentPageCallback("2")}
+          className="page-square-button-2"
+          tabIndex="2"
+        >
           2
         </div>
       </div>
