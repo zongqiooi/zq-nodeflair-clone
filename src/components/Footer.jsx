0b06a06806335487d96776bed1a0d7ebe5b27b45
zongqiooi <huro710@hotmail.com>
2023-08-21 03:01:33 +0800

    chore: update footer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,16 @@
-import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p className="icons">
-        <div className="linkedin">
-          <a href="https://www.linkedin.com/in/zongqiooi/">
-            <AiFillLinkedin style={{ color: "black" }} />
-          </a>
-        </div>
-        <div className="github">
+      <span className="icons">
+        <span className="github">
           <a href="https://github.com/zongqiooi/zq-nodeflair-clone">
             <AiFillGithub style={{ color: "black" }} />
           </a>
-        </div>
-      </p>
-      <p>Made with ❤️ by Zong Qi.</p>
+        </span>
+      </span>
+      <p>Made with ❤️ by Zong Qi </p>
     </div>
   );
 };
